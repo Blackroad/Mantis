43a3d078c31c0e016e366d7447c58d247181fabd
Blackroad <VladVell@gmail.com>
2016-10-19 18:03:43 +0300

    config added

---
+++ resources/config_inc.php
@@ -0,0 +1,11 @@
+<?php
+$g_hostname               = 'localhost';
+$g_db_type                = 'mysqli';
+$g_database_name          = 'bugtracker';
+$g_db_username            = 'root';
+$g_db_password            = '';
+
+$g_default_timezone       = 'Europe/Berlin';
+
+$g_crypto_master_salt     = 'mznRegPY81Nk60l7HN4IY+PI9JanBeabz8UekxJv+mY=';
+$g_signup_use_captcha     = 'off';
